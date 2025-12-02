b1012130c1e0d94c63ac1bfd952ed12b7cbafb6d
Julia Bush <j.elizabethbush@gmail.com>
2025-12-03 00:07:06 +0100

    add: adding typing effect for page

--- app/page.tsx
+++ app/page.tsx
@@ -20,5 +20,17 @@ export default function HomePage() {
   const [displayedText, setDisplayedText] = useState("");
   const indexRef = useRef(0);
 
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (indexRef.current < fullText.length) {
+        setDisplayedText(fullText.slice(0, indexRef.current + 1));
+        indexRef.current += 1;
+      } else {
+        clearInterval(interval);
+      }
+    }, 100);
+    return () => clearInterval(interval);
+  }, []);
+
   return <div></div>;
 }
