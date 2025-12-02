8898940961f5b94a1d4aedb97783fdc97d26a659
Julia Bush <j.elizabethbush@gmail.com>
2025-12-02 23:41:08 +0100

    add: including imports for the homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
 "use client";
 
+import { useEffect, useRef, useState } from "react";
+import { motion } from "framer-motion";
+import TarotInput from "@/components/tarot_input";
+import VideoCard from "@/components/video_card";
+
 export default function HomePage() {
   return <div></div>;
 }
