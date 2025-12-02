7585fba2c76eb19f6a859ee89ac478fdc67097ee
Julia Bush <j.elizabethbush@gmail.com>
2025-12-03 00:29:51 +0100

    add: creating a video element

--- components/video_card.tsx
+++ components/video_card.tsx
@@ -8,7 +8,16 @@ interface VideoCradProps {
 export default function VideoCard({ src, videRef }: VideoCardProps) {
   return (
     <div className="flex flex-col items-center">
-      <div className="w-[300px] h-[500px] rounded-xl overflow-hidden shadow-xl border-4 border-blue-300"></div>
+      <div className="w-[300px] h-[500px] rounded-xl overflow-hidden shadow-xl border-4 border-blue-300">
+        <video
+          ref={videoRef}
+          src={src}
+          loop
+          muted
+          playsInline
+          className="w-full h-full object-cover"
+        />
+      </div>
     </div>
   );
 }
