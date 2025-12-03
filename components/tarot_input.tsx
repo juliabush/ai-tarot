09d51f14bed6bbaffb6767f6a801da0ab0542341
Julia Bush <j.elizabethbush@gmail.com>
2025-12-03 02:47:10 +0100

    add: adding use client and imports

---
+++ components/tarot_input.tsx
@@ -0,0 +1,8 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import tarotCards from "@/tarot-json/tarot-loop.json";
+import TarotCardDisplay from "./tarot_card_display";
+import ResponseModal from "./response_modal";
+import { pickRandomCards } from "../lib/tarot_utils";
+import { typeWriterAppend } from "../lib/typewriter";
