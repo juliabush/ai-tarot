e775e376c2cc1618fa42d57fe2a4d3e67a57f763
Julia Bush <j.elizabethbush@gmail.com>
2025-12-03 02:34:50 +0100

    style: add styling for tarot card images and names

--- components/tarot_card_display.tsx
+++ components/tarot_card_display.tsx
@@ -8,9 +8,15 @@ export default function TarotCardDisplay({
   return (
     <div className="flex justify-center gap-6 mb-6 flex-wrap">
       {selectedCards.map((card) => (
-        <div key={card.name}>
-          <img src={card.url} alt={card.name} />
-          <span>{card.name}</span>
+        <div key={card.name} className="flex flex-col items-center">
+          <img
+            src={card.url}
+            alt={card.name}
+            className="w-36 h-56 object-cover rounded-lg border-2 border-purple-600"
+          />
+          <span className="mt-2 font-semibold text-purple-600 text-center">
+            {card.name}
+          </span>
         </div>
       ))}
     </div>
