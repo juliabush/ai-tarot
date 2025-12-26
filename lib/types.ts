0aa13e924a17590bcfa814fb7287de1cc10c9a6c
Julia Bush <j.elizabethbush@gmail.com>
2025-12-26 19:06:10 +0100

    update: moving props from video card and response modal

--- lib/types.ts
+++ lib/types.ts
@@ -2,3 +2,16 @@ export interface TarotCard {
   name: string;
   url: string;
 }
+
+export interface VideoCardProps {
+  src: string;
+  videoRef: React.Ref<HTMLVideoElement>;
+}
+
+export interface ResponseModalProps {
+  question: string;
+  displayedText: string;
+  selectedCards: TarotCard[];
+  loading: boolean;
+  onClose: () => void;
+}
